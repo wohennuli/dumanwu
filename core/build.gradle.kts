31c17fb9f8ab923e612163ad9b5c8a68fa0c6678
xiaolan <103926807+wohennuli@users.noreply.github.com>
2026-03-16 15:28:30 +0800

    Add files via upload

---
+++ core/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+    id("keiyoushi.lint")
+}
+
+android {
+    compileSdk = AndroidConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AndroidConfig.minSdk
+    }
+
+    namespace = "keiyoushi.core"
+
+    buildFeatures {
+        resValues = false
+        shaders = false
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.add("-opt-in=kotlinx.serialization.ExperimentalSerializationApi")
+    }
+}
+
+dependencies {
+    compileOnly(versionCatalogs.named("libs").findBundle("common").get())
+}
